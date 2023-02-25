c9659daee579a7ea12c66314239b554eeb802f11
nadimee <115081345+nadimee@users.noreply.github.com>
2023-02-25 22:58:33 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ app.use(morgan('dev'))//app use morgan in logging requests
 app.use('/api/workouts',workoutRoutes)//express app use workoutRoutes and give it the path /api/workouts
 app.use('/api/user',userRoutes)//express app use userRoutes and give it the path /api/user
 
+app.use(express.static(path.join(__dirname+'/frontend', 'build')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname+'/frontend', 'build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.set('strictQuery', false)
