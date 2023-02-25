46b662aed1c3b540904cecb1a3a75ed82054caff
nadimee <115081345+nadimee@users.noreply.github.com>
2023-02-25 21:28:45 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,9 @@ import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <AuthContextProvider>
       <WorkoutContextProvider>
         <App />
       </WorkoutContextProvider>
     </AuthContextProvider>
-  </React.StrictMode>
 );
