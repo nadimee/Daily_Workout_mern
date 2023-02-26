f10dcf5a6ff57f3c1df072dcdd88ba1a568192aa
nadimee <115081345+nadimee@users.noreply.github.com>
2023-02-26 05:07:20 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')/*map the model attributes and requirements 
                                       and documents in our database, and make queries to retrieve info from the database*/
 const workoutRoutes = require('./routes/workoutsRoutes')//route that app will use
 const userRoutes = require('./routes/userRoutes')//route that app will use
-const path = require('path');
+
 
 // express app
 const app = express();//app will get the request from a specefic url and response to specific url
@@ -20,14 +20,11 @@ app.use('/api/workouts',workoutRoutes)//express app use workoutRoutes and give i
 app.use('/api/user',userRoutes)//express app use userRoutes and give it the path /api/user
 
 
+app.use(express.static('daily_workout_mern/frontend/public'));
 
-
-// Serve static files from the client directory
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-// Route handler for the home page
+// Handle GET requests for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'frontend','index.html'));
+  res.sendFile('index.html');
 });
 
 
