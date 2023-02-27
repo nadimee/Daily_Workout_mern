f5244f60e3739d05d9b60d5e50b0b03879a5cc17
nadimee <115081345+nadimee@users.noreply.github.com>
2023-02-27 14:10:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/api/workouts',workoutRoutes)//express app use workoutRoutes and give i
 app.use('/api/user',userRoutes)//express app use userRoutes and give it the path /api/user
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/frontend/build/index.html');
+  res.sendFile(__dirname+'/../frontend/build/index.html');
 });
 
 
