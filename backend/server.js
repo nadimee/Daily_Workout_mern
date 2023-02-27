8be0bccc3712289c1aaa4ed3825c5c4d54198871
nadimee <115081345+nadimee@users.noreply.github.com>
2023-02-27 01:42:09 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ const mongoose = require('mongoose')/*map the model attributes and requirements 
                                       and documents in our database, and make queries to retrieve info from the database*/
 const workoutRoutes = require('./routes/workoutsRoutes')//route that app will use
 const userRoutes = require('./routes/userRoutes')//route that app will use
-const indexHtml = require('../frontend/build/index.html')
+
 
 // express app
 const app = express();//app will get the request from a specefic url and response to specific url
-
+app.use(express.static('../frontend'))//serve static files 
+const indexHtml = require('../frontend/build/index.html')
 // middlewares
 app.use(express.json())//our app will use json format in communication
-app.use(express.static('../frontend'))//serve static files 
 app.use(morgan('dev'))//app use morgan in logging requests
 
 // routes
